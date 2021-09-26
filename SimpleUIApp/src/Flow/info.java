22e2e456bb6845e7d306f0a18a13d7ab2ec09a11
Achuthan Ram <ram.a@northeastern.edu>
2021-09-26 08:40:02 -0400

    Simple UI application
    Takes different inputs from user, displays collected information based on validations.

---
+++ SimpleUIApp/src/Flow/info.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Flow;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author ram.a
+ */
+import java.util.regex.*;
+import javax.swing.JOptionPane;
+public class info {
+    
+    private String name;
+    private String country;
+    private Date   dob;
+    private String phone;
+    private String fax;
+    private String email;
+    private String ssn;
+    private String healthno;
+    private String bankac;
+    private String license;
+    private String vehicle;
+    private String linkedin;
+    private String ip;
+    private String mrn;
+    private String img_src;
+    private String device;
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+    
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+    
+
+    public String getImg_src() {
+        return img_src;
+    }
+
+    public void setImg_src(String img_src) {
+        this.img_src = img_src;
+    }
+ 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+           
+        this.email = email;
+                  
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getHealthno() {
+        return healthno;
+    }
+
+    public void setHealthno(String healthno) {
+        this.healthno = healthno;
+    }
+
+    public String getBankac() {
+        return bankac;
+    }
+
+    public void setBankac(String bankac) {
+        this.bankac = bankac;
+    }
+    
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMrn() {
+        return mrn;
+    }
+
+    public void setMrn(String mrn) {
+        this.mrn = mrn;
+    }
+
+    
+}
+
+
